7b0a0907acc6e8c6186a0a79d34ba1759469d9ae
LordKalou <pascal.ruffie.pro@gmail.com>
2022-06-12 18:25:23 +0200

    fichier de test Kalou

---
+++ testKalou.py
@@ -0,0 +1,7 @@
+##########################################################################################
+# Pour faire une interface graphique avec pyQt, il faut au préalable installer:
+# pip install pyqt5
+##########################################################################################
+
+
+print("Projet d'Amaury et de Pascal")
